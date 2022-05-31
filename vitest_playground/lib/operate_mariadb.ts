772cbb2b181d96508b1bd78a6f546f89d3e6de6d
Akira Kashihara <akira.kashihara@hotmail.com>
2022-05-31 15:10:53 +0900

    Establish the environment for vitest.

---
+++ vitest_playground/lib/operate_mariadb.ts
@@ -0,0 +1,34 @@
+import mariadb, { Connection, Pool } from "mariadb";
+export default class OperateMariadb {
+  pool: Pool;
+  connection: Connection | undefined;
+  constructor() {
+    this.pool = mariadb.createPool({
+      host: process.env.MARIADB_HOST,
+      user: process.env.MARIADB_USER,
+      password: process.env.MARIADB_PASSWORD,
+      database: process.env.MARIADB_DATABASE,
+    });
+  }
+  async checkCardName(value: string): Promise<boolean> {
+    try {
+      const con = await this.pool.getConnection();
+      const result = await con.query(
+        "select * from cards where card_name = ?",
+        [value]
+      );
+      con.end();
+      delete result.meta;
+      const lengthObj = Object.keys(result).length;
+      if (lengthObj > 0) {
+        return true;
+      } else if (lengthObj === 0) {
+        return false;
+      } else {
+        throw new Error("Object length is invalid in checkCardName.");
+      }
+    } catch (err: any) {
+      throw new Error(err);
+    }
+  }
+}
